8b4870e81920df51ef9f9ac81ce871ce81b992ea
faysalana <141560288+faysalana@users.noreply.github.com>
2023-09-09 17:32:45 +0300

    Add files via upload

---
+++ SS_EO_De.py
@@ -0,0 +1,21 @@
+def selection_sort(list1):
+  border=0
+  while border<len(list1)-1:
+    minIndex=border
+    for i in range(border,len(list1)): # looking for minimum
+        if (list1[i] % 2 == 0) and (list1[i] > list1[minIndex] or list1[minIndex] % 2 != 0):
+ #  found a smaller number, update the index
+            minIndex=i
+    # swap the min element, with the element at my border
+    temp=list1[border]
+    list1[border]=list1[minIndex]
+    list1[minIndex]=temp
+    
+    print(list1)
+
+    border+=1
+
+
+list1=[2,0,3,0,10,-1,6]
+
+selection_sort(list1)
---
+++ merge_sort_desc.py
@@ -0,0 +1,43 @@
+def merge_descending(list1, start, end):  # O(n)
+    mid = (start + end) // 2
+
+    # The first sublist starts from start -> mid, the second sublist starts from mid+1 -> end
+    merged_list = []
+
+    index1 = start  # Keeps track of the elements added from the first sublist
+    index2 = mid + 1  # Keeps track of the elements added from the second sublist
+
+    while index1 <= mid and index2 <= end:
+
+        if list1[index1] >= list1[index2]:  # Reversed comparison for descending order
+             #hon lfr2    ^
+            merged_list.append(list1[index1])  # Copying from the first sublist
+            index1 += 1
+        else:
+            merged_list.append(list1[index2])  # Copying from the second sublist
+            index2 += 1
+
+    while index1 <= mid:
+        merged_list.append(list1[index1])  # Copy the remaining elements of the first sublist
+        index1 += 1
+
+    while index2 <= end:
+        merged_list.append(list1[index2])  # Copy the remaining elements of the second sublist
+        index2 += 1
+
+    # Update the original list with the sorted descending list
+    list1[start:end + 1] = merged_list
+
+def mergesort_descending(list1, start, end):  # O(n*log(n)), n being the length of the list
+    if start == end:  # Base case: sublist of length == 1
+        return
+
+    mid = (start + end) // 2
+    mergesort_descending(list1, start, mid)  # Divide the list into two halves
+    mergesort_descending(list1, mid + 1, end)
+
+    merge_descending(list1, start, end)  # Merge the two halves in descending order
+
+list1 = [2, 0, 3, 0, 10, -1, 6]
+mergesort_descending(list1, 0, len(list1) - 1)
+print(list1)
